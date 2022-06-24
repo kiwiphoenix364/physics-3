a8663ca38fcc2cde770f3815dd7eebbb90bf5b25
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-06-24 17:53:35 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -64,7 +64,7 @@ function GenerateCollision () {
                 for (let index32 = 0; index32 <= collisionImages.width; index32++) {
                     for (let index2 = 0; index2 <= collisionImages.height; index2++) {
                         if (0 != collisionImages.getPixel(index32, index2)) {
-                            TileCollisionArrayX.push(value2.column * 16 + index32)
+                            TileCollisionArrayX.push(value2.column * 16 + index32 + 1)
                             TileCollisionArrayY.push(value2.row * 16 + index2 + 1)
                         }
                     }
@@ -76,8 +76,8 @@ function GenerateCollision () {
 controller.menu.onEvent(ControllerButtonEvent.Pressed, function () {
     stats.turnStats(true)
 })
-let prevy = 0
 let prevx = 0
+let prevy = 0
 let cany = false
 let canx = false
 let cury = 0
@@ -90,38 +90,38 @@ TileCollisionArrayY = [0]
 TileCollisionArrayX = [0]
 tiles.setCurrentTilemap(tilemap`level2`)
 let mySprite = sprites.create(img`
-    22222222222222222222222222222222
-    22222222222222222222222222222222
-    22222222222222222222222222222222
-    22222222222222222222222222222222
-    22222222222222222222222222222222
-    22222222222222222222222222222222
-    22222222222222222222222222222222
-    22222222222222222222222222222222
-    22222222222222222222222222222222
-    22222222222222222222222222222222
-    22222222222222222222222222222222
-    22222222222222222222222222222222
-    22222222222222222222222222222222
-    22222222222222222222222222222222
-    22222222222222222222222222222222
-    22222222222222222222222222222222
-    22222222222222222222222222222222
-    22222222222222222222222222222222
-    22222222222222222222222222222222
-    22222222222222222222222222222222
-    22222222222222222222222222222222
-    22222222222222222222222222222222
-    22222222222222222222222222222222
-    22222222222222222222222222222222
-    22222222222222222222222222222222
-    22222222222222222222222222222222
-    22222222222222222222222222222222
-    22222222222222222222222222222222
-    22222222222222222222222222222222
-    22222222222222222222222222222222
-    22222222222222222222222222222222
-    22222222222222222222222222222222
+    22222222222222222222222222222223
+    22222222222222222222222222222232
+    22222222222222222222222222222322
+    22222222222222222222222222223222
+    22222222222222222222222222232222
+    22222222222222222222222222322222
+    22222222222222222222222223222222
+    22222222222222222222222232222222
+    22222222222222222222222322222222
+    22222222222222222222223222222222
+    22222222222222222222232222222222
+    22222222222222222222322222222222
+    22222222222222222223222222222222
+    22222222222222222232222222222222
+    22222222222222222322222222222222
+    22222222222222223222222222222222
+    22222222222222232222222222222222
+    22222222222222322222222222222222
+    22222222222223222222222222222222
+    22222222222232222222222222222222
+    22222222222322222222222222222222
+    22222222223222222222222222222222
+    22222222232222222222222222222222
+    22222222322222222222222222222222
+    22222223222222222222222222222222
+    22222232222222222222222222222222
+    22222322222222222222222222222222
+    22223222222222222222222222222222
+    22232222222222222222222222222222
+    22322222222222222222222222222222
+    23222222222222222222222222222222
+    32222222222222222222222222222222
     `, SpriteKind.Player)
 scene.cameraFollowSprite(mySprite)
 let mySprite3 = sprites.create(img`
@@ -130,17 +130,7 @@ let mySprite3 = sprites.create(img`
 mySprite3.setFlag(SpriteFlag.Invisible, true)
 mySprite.y = 128
 controller.moveSprite(mySprite, 30, 30)
-let mySpritePixels: number[] = []
-for (let index = 0; index <= mySprite.width; index++) {
-    for (let index2 = 0; index2 <= mySprite.height; index2++) {
-        let list: boolean[] = []
-        if (mySprite.image.getPixel(index, index2) > 0) {
-            list.push(true)
-        } else {
-            list.push(false)
-        }
-    }
-}
+let list: number[] = []
 GenerateCollision()
 game.onUpdate(function () {
     curx = mySprite.x
@@ -149,10 +139,10 @@ game.onUpdate(function () {
     canx = true
     cany = true
     for (let index4 = 0; index4 <= TileCollisionArrayX.length; index4++) {
-        if (TileCollisionArrayY[index4] >= mySprite.top && TileCollisionArrayY[index4] <= mySprite.bottom && (TileCollisionArrayX[index4] >= mySprite.left && TileCollisionArrayX[index4] <= mySprite.right)) {
-            if (true) {
+        if (TileCollisionArrayY[index4] >= mySprite.top && TileCollisionArrayX[index4] <= mySprite.bottom && (TileCollisionArrayX[index4] >= mySprite.left && TileCollisionArrayX[index4] <= mySprite.right)) {
+            mySprite.y = prevy
+            if (mySprite.image.getPixel(TileCollisionArrayX[index4] - mySprite.left, TileCollisionArrayY[index4] - mySprite.top) != 0) {
                 mySprite.x = prevx
-                mySprite.y = prevy
             }
         }
     }
