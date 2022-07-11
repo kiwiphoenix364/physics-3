cac9c0ceba95e7896ff06d9d63a063a7e80f6794
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-07-11 18:12:37 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -65,7 +65,7 @@ function GenerateCollision () {
                     for (let index2 = 0; index2 <= collisionImages.height; index2++) {
                         if (0 != collisionImages.getPixel(index32, index2)) {
                             TileCollisionArrayX.push(value2.column * 16 + index32 + 1)
-                            TileCollisionArrayY.push(value2.row * 16 + index2 + 1)
+                            TileCollisionArrayY.push(value2.row * 16 + index2)
                         }
                     }
                 }
@@ -76,9 +76,9 @@ function GenerateCollision () {
 controller.menu.onEvent(ControllerButtonEvent.Pressed, function () {
     stats.turnStats(true)
 })
+let cany = 0
+let canx = 0
 let repeat = 0
-let prevy = 0
-let prevx = 0
 let cury = 0
 let curx = 0
 let item: tiles.Location[] = []
@@ -122,6 +122,8 @@ let mySprite = sprites.create(img`
     23222222222222222222222222222222
     32222222222222222222222222222222
     `, SpriteKind.Player)
+let prevx = mySprite.x
+let prevy = mySprite.y
 scene.cameraFollowSprite(mySprite)
 let mySprite3 = sprites.create(img`
     c 
@@ -129,26 +131,38 @@ let mySprite3 = sprites.create(img`
 mySprite3.setFlag(SpriteFlag.Invisible, true)
 mySprite.y = 128
 controller.moveSprite(mySprite, 100, 100)
-let list: number[] = []
 GenerateCollision()
 game.onUpdate(function () {
     curx = mySprite.x
     cury = mySprite.y
-    mySprite.setFlag(SpriteFlag.GhostThroughWalls, false)
-    for (let index4 = 0; index4 <= TileCollisionArrayX.length; index4++) {
-        if (TileCollisionArrayY[index4] >= mySprite.top && TileCollisionArrayY[index4] <= mySprite.bottom && (TileCollisionArrayX[index4] >= mySprite.left && TileCollisionArrayX[index4] <= mySprite.right)) {
-            if (mySprite.image.getPixel(TileCollisionArrayX[index4] - mySprite.left, TileCollisionArrayY[index4] - mySprite.top) != 0) {
-                mySprite.setPosition(prevx, prevy)
-                repeat = curx - prevx + (cury - prevy)
-                for (let index = 0; index <= repeat; index++) {
-                    if (mySprite.image.getPixel(TileCollisionArrayX[index4] - mySprite.left, TileCollisionArrayY[index4] - mySprite.top) == 0) {
-                        mySprite.x += Math.constrain((curx - prevx) / repeat, -1, 1)
-                        mySprite.y += Math.constrain((cury - prevy) / repeat, -1, 1)
+    mySprite.setPosition(prevx, prevy)
+    repeat = Math.abs(curx - prevx) + Math.abs(cury - prevy)
+    canx = 1
+    cany = 1
+    for (let index = 0; index <= repeat; index++) {
+        if (repeat > 0) {
+            let index4 = 0
+            if (canx == 1) {
+                mySprite.x += (curx - prevx) / repeat
+                for (let index2 = 0; index2 <= TileCollisionArrayX.length; index2++) {
+                    if (canx == 1) {
+                        if (mySprite.image.getPixel(TileCollisionArrayX[index4] - mySprite.left, TileCollisionArrayY[index4] - mySprite.top) != 0) {
+                            mySprite.x += 0 - (curx - prevx) / repeat
+                            canx = 0
+                        }
                     }
-                    mySprite.x += 0 - Math.constrain((curx - prevx) / repeat, -1, 1)
-                    mySprite.y += 0 - Math.constrain((cury - prevy) / repeat, -1, 1)
                 }
-                break;
+            }
+            if (cany == 1) {
+                mySprite.y += (cury - prevy) / repeat
+                for (let index2 = 0; index2 <= TileCollisionArrayY.length; index2++) {
+                    if (cany == 1) {
+                        if (mySprite.image.getPixel(TileCollisionArrayX[index4] - mySprite.left, TileCollisionArrayY[index4] - mySprite.top) != 0) {
+                            mySprite.y += 0 - (cury - prevy) / repeat
+                            cany = 0
+                        }
+                    }
+                }
             }
         }
     }
