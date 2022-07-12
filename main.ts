16046e8ea111059039cc5ee7dffcc6afd037a98e
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-07-12 18:11:10 -0400

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -3,68 +3,31 @@ namespace SpriteKind {
 }
 function GenerateCollision () {
     timer.background(function () {
-        for (let index6 = 0; index6 <= 2; index6++) {
-            // put the tilemap locations that correspond to the order of your index of regular platform images and collisions
-            if (index6 == 0) {
-                collisionImages = img`
-                    ..............................cc
-                    ............................cc..
-                    ..........................cc....
-                    ........................cc......
-                    ......................cc........
-                    ....................cc..........
-                    ..................cc............
-                    ................cc..............
-                    ..............cc................
-                    ............cc..................
-                    ..........cc....................
-                    ........cc......................
-                    ......cc........................
-                    ....cc..........................
-                    ..cc............................
-                    cc..............................
-                    `
-            } else if (index6 == 1) {
-                collisionImages = img`
-                    c . . . . . . . . . . . . . . . 
-                    . c . . . . . . . . . . . . . . 
-                    . . c . . . . . . . . . . . . . 
-                    . . . c . . . . . . . . . . . . 
-                    . . . . c . . . . . . . . . . . 
-                    . . . . . c . . . . . . . . . . 
-                    . . . . . . c . . . . . . . . . 
-                    . . . . . . . c . . . . . . . . 
-                    . . . . . . . . c . . . . . . . 
-                    . . . . . . . . . c . . . . . . 
-                    . . . . . . . . . . c . . . . . 
-                    . . . . . . . . . . . c . . . . 
-                    . . . . . . . . . . . . c . . . 
-                    . . . . . . . . . . . . . c . . 
-                    . . . . . . . . . . . . . . c . 
-                    . . . . . . . . . . . . . . . c 
-                    `
-            } else if (index6 == 2) {
-            	
-            } else {
-            	
-            }
-            // put the tilemap locations that correspond to the order of your index of regular platform images and collisions
-            if (index6 == 0) {
-                item = tiles.getTilesByType(assets.tile`myTile13`)
-            } else if (index6 == 1) {
-                item = tiles.getTilesByType(assets.tile`myTile`)
-            } else if (index6 == 2) {
-            	
-            } else {
-            	
-            }
-            for (let value2 of item) {
-                for (let index32 = 0; index32 <= collisionImages.width; index32++) {
-                    for (let index2 = 0; index2 <= collisionImages.height; index2++) {
-                        if (0 != collisionImages.getPixel(index32, index2)) {
-                            TileCollisionArrayX.push(value2.column * 16 + index32 + 1)
-                            TileCollisionArrayY.push(value2.row * 16 + index2)
-                        }
+        collisionImages = img`
+            ..............................cc
+            ............................cc..
+            ..........................cc....
+            ........................cc......
+            ......................cc........
+            ....................cc..........
+            ..................cc............
+            ................cc..............
+            ..............cc................
+            ............cc..................
+            ..........cc....................
+            ........cc......................
+            ......cc........................
+            ....cc..........................
+            ..cc............................
+            cc..............................
+            `
+        item = tiles.getTilesByType(assets.tile`myTile13`)
+        for (let value2 of item) {
+            for (let index32 = 0; index32 <= collisionImages.width; index32++) {
+                for (let index2 = 0; index2 <= collisionImages.height; index2++) {
+                    if (0 != collisionImages.getPixel(index32, index2)) {
+                        TileCollisionArrayX.push(value2.column * 16 + index32 + 1)
+                        TileCollisionArrayY.push(value2.row * 16 + index2)
                     }
                 }
             }
@@ -141,14 +104,15 @@ game.onUpdate(function () {
     cany = 1
     for (let index = 0; index <= repeat; index++) {
         if (repeat > 0) {
-            let index4 = 0
             if (canx == 1) {
                 mySprite.x += (curx - prevx) / repeat
                 for (let index2 = 0; index2 <= TileCollisionArrayX.length; index2++) {
                     if (canx == 1) {
-                        if (mySprite.image.getPixel(TileCollisionArrayX[index4] - mySprite.left, TileCollisionArrayY[index4] - mySprite.top) != 0) {
-                            mySprite.x += 0 - (curx - prevx) / repeat
-                            canx = 0
+                        if (TileCollisionArrayX[index2] - mySprite.left >= 0 && TileCollisionArrayX[index2] - mySprite.left < mySprite.width && (TileCollisionArrayY[index2] - mySprite.top >= 0 && TileCollisionArrayY[index2] - mySprite.top < mySprite.height)) {
+                            if (mySprite.image.getPixel(TileCollisionArrayX[index2] - mySprite.left, TileCollisionArrayY[index2] - mySprite.top) != 0) {
+                                mySprite.x += 0 - (curx - prevx) / repeat
+                                canx = 0
+                            }
                         }
                     }
                 }
@@ -157,9 +121,11 @@ game.onUpdate(function () {
                 mySprite.y += (cury - prevy) / repeat
                 for (let index2 = 0; index2 <= TileCollisionArrayY.length; index2++) {
                     if (cany == 1) {
-                        if (mySprite.image.getPixel(TileCollisionArrayX[index4] - mySprite.left, TileCollisionArrayY[index4] - mySprite.top) != 0) {
-                            mySprite.y += 0 - (cury - prevy) / repeat
-                            cany = 0
+                        if (TileCollisionArrayX[index2] - mySprite.left >= 0 && TileCollisionArrayX[index2] - mySprite.left < mySprite.width && (TileCollisionArrayY[index2] - mySprite.top >= 0 && TileCollisionArrayY[index2] - mySprite.top < mySprite.height)) {
+                            if (mySprite.image.getPixel(TileCollisionArrayX[index2] - mySprite.left, TileCollisionArrayY[index2] - mySprite.top) != 0) {
+                                mySprite.y += 0 - (cury - prevy) / repeat
+                                cany = 0
+                            }
                         }
                     }
                 }
