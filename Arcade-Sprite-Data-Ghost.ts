eadb5089279b4367225e508d7399826e2e571f2e
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-07-13 12:49:15 -0400

    Restore '7/12/2022, 9:55:52 PM Update main.ts, tilemap.g.jres, tilemap.g.ts'

---
+++ Arcade-Sprite-Data-Ghost.ts
@@ -0,0 +1,58 @@
+// Copy of Arcade-Sprite-Data, used in this extension, but will not show up in the toolbox.
+namespace sprites {
+    export function setDataNumber(sprite: Sprite, name: string, value: number) {
+        if (!sprite || !name) return;
+        const d = sprite.data;
+        d[name] = value;
+    }
+    export function changeDataNumberBy(sprite: Sprite, name: string, value: number) {
+        if (!sprite || !name) return;
+        const d = sprite.data;
+        d[name] = (d[name] || 0) + value;
+    }
+    export function readDataNumber(sprite: Sprite, name: string): number {
+        if (!sprite || !name) return 0;
+        const d = sprite.data;
+        return d[name] as number;
+    }
+    export function setDataString(sprite: Sprite, name: string, value: string) {
+        if (!sprite || !name) return;
+        const d = sprite.data;
+        d[name] = value;
+    }
+    export function readDataString(sprite: Sprite, name: string): string {
+        if (!sprite || !name) return "";
+        const d = sprite.data;
+        return d[name] as string;
+    }
+    export function setDataBoolean(sprite: Sprite, name: string, value: boolean) {
+        if (!sprite || !name) return;
+        const d = sprite.data;
+        d[name] = !!value;
+    }
+    export function readDataBoolean(sprite: Sprite, name: string): boolean {
+        if (!sprite || !name) return false;
+        const d = sprite.data;
+        return !!d[name];
+    }
+    export function setDataSprite(sprite: Sprite, name: string, value: Sprite) {
+        if (!sprite || !name) return;
+        const d = sprite.data;
+        d[name] = value;
+    }
+    export function readDataSprite(sprite: Sprite, name: string): Sprite {
+        if (!sprite || !name) return undefined;
+        const d = sprite.data;
+        return d[name] as Sprite;
+    }
+    export function setDataImage(sprite: Sprite, name: string, value: Image) {
+        if (!sprite || !name) return;
+        const d = sprite.data;
+        d[name] = value;
+    }
+    export function readDataImage(sprite: Sprite, name: string): Image {
+        if (!sprite || !name) return undefined;
+        const d = sprite.data;
+        return d[name] as Image;
+    }
+}
--- main.ts
+++ main.ts
@@ -1,137 +1,81 @@
 namespace SpriteKind {
-    export const movingplatform = SpriteKind.create()
+    export const AffectedByPhysics = SpriteKind.create()
 }
-function GenerateCollision () {
-    timer.background(function () {
-        collisionImages = img`
-            ..............................cc
-            ............................cc..
-            ..........................cc....
-            ........................cc......
-            ......................cc........
-            ....................cc..........
-            ..................cc............
-            ................cc..............
-            ..............cc................
-            ............cc..................
-            ..........cc....................
-            ........cc......................
-            ......cc........................
-            ....cc..........................
-            ..cc............................
-            cc..............................
-            `
-        item = tiles.getTilesByType(assets.tile`myTile13`)
-        for (let value2 of item) {
-            for (let index32 = 0; index32 <= collisionImages.width; index32++) {
-                for (let index2 = 0; index2 <= collisionImages.height; index2++) {
-                    if (0 != collisionImages.getPixel(index32, index2)) {
-                        TileCollisionArrayX.push(value2.column * 16 + index32 + 1)
-                        TileCollisionArrayY.push(value2.row * 16 + index2)
-                    }
-                }
-            }
-        }
-    })
-}
-controller.menu.onEvent(ControllerButtonEvent.Pressed, function () {
-    stats.turnStats(true)
-})
 let cany = 0
 let canx = 0
 let repeat = 0
 let cury = 0
 let curx = 0
-let item: tiles.Location[] = []
-let collisionImages: Image = null
-let TileCollisionArrayX: number[] = []
 let TileCollisionArrayY: number[] = []
+let TileCollisionArrayX: number[] = []
+let collisionImages: Image = null
 TileCollisionArrayY = [0]
 TileCollisionArrayX = [0]
 TileCollisionArrayY = []
 TileCollisionArrayX = []
-tiles.setCurrentTilemap(tilemap`level2`)
-let mySprite = sprites.create(img`
-    22222222222222222222222222222223
-    22222222222222222222222222222232
-    22222222222222222222222222222322
-    22222222222222222222222222223222
-    22222222222222222222222222232222
-    22222222222222222222222222322222
-    22222222222222222222222223222222
-    22222222222222222222222232222222
-    22222222222222222222222322222222
-    22222222222222222222223222222222
-    22222222222222222222232222222222
-    22222222222222222222322222222222
-    22222222222222222223222222222222
-    22222222222222222232222222222222
-    22222222222222222322222222222222
-    22222222222222223222222222222222
-    22222222222222232222222222222222
-    22222222222222322222222222222222
-    22222222222223222222222222222222
-    22222222222232222222222222222222
-    22222222222322222222222222222222
-    22222222223222222222222222222222
-    22222222232222222222222222222222
-    22222222322222222222222222222222
-    22222223222222222222222222222222
-    22222232222222222222222222222222
-    22222322222222222222222222222222
-    22223222222222222222222222222222
-    22232222222222222222222222222222
-    22322222222222222222222222222222
-    23222222222222222222222222222222
-    32222222222222222222222222222222
-    `, SpriteKind.Player)
-let prevx = mySprite.x
-let prevy = mySprite.y
-scene.cameraFollowSprite(mySprite)
-let mySprite3 = sprites.create(img`
-    c 
-    `, SpriteKind.Player)
-mySprite3.setFlag(SpriteFlag.Invisible, true)
-mySprite.y = 128
-controller.moveSprite(mySprite, 100, 100)
-GenerateCollision()
-game.onUpdate(function () {
-    curx = mySprite.x
-    cury = mySprite.y
-    mySprite.setPosition(prevx, prevy)
-    repeat = Math.abs(curx - prevx) + Math.abs(cury - prevy)
-    canx = 1
-    cany = 1
-    for (let index = 0; index <= repeat; index++) {
-        if (repeat > 0) {
-            if (canx == 1) {
-                mySprite.x += (curx - prevx) / repeat
-                for (let index2 = 0; index2 <= TileCollisionArrayX.length; index2++) {
-                    if (canx == 1) {
-                        if (TileCollisionArrayX[index2] - mySprite.left >= 0 && TileCollisionArrayX[index2] - mySprite.left < mySprite.width && (TileCollisionArrayY[index2] - mySprite.top >= 0 && TileCollisionArrayY[index2] - mySprite.top < mySprite.height)) {
-                            if (mySprite.image.getPixel(TileCollisionArrayX[index2] - mySprite.left, TileCollisionArrayY[index2] - mySprite.top) != 0) {
-                                mySprite.x += 0 - (curx - prevx) / repeat
-                                canx = 0
-                            }
-                        }
-                    }
-                }
-            }
-            if (cany == 1) {
-                mySprite.y += (cury - prevy) / repeat
-                for (let index2 = 0; index2 <= TileCollisionArrayY.length; index2++) {
-                    if (cany == 1) {
-                        if (TileCollisionArrayX[index2] - mySprite.left >= 0 && TileCollisionArrayX[index2] - mySprite.left < mySprite.width && (TileCollisionArrayY[index2] - mySprite.top >= 0 && TileCollisionArrayY[index2] - mySprite.top < mySprite.height)) {
-                            if (mySprite.image.getPixel(TileCollisionArrayX[index2] - mySprite.left, TileCollisionArrayY[index2] - mySprite.top) != 0) {
-                                mySprite.y += 0 - (cury - prevy) / repeat
-                                cany = 0
-                            }
-                        }
+namespace MiniTilemaps {
+    //% block="Generate mini tilemap on all $selected tiles with collision $collisionImg"
+    export function GenerateCollision(selected: any, collisionImg: any) {
+        collisionImages = collisionImg
+        let item = tiles.getTilesByType(selected)
+        for (let value2 of item) {
+            for (let index32 = 0; index32 <= collisionImages.width; index32++) {
+                for (let index23 = 0; index23 <= collisionImages.height; index23++) {
+                    if (0 != collisionImages.getPixel(index32, index23)) {
+                        TileCollisionArrayX.push(value2.column * 16 + index32)
+                        TileCollisionArrayY.push(value2.row * 16 + index23)
                     }
                 }
             }
         }
     }
-    prevx = mySprite.x
-    prevy = mySprite.y
+    //% block="Clear all mini tilemaps"
+    export function ClearAll () {
+    TileCollisionArrayX = []
+    TileCollisionArrayY = []
+}
+}
+game.onUpdate(function () {
+    if (TileCollisionArrayX.length != 0) {
+        for (let mySprite of sprites.allOfKind(SpriteKind.AffectedByPhysics)) {
+            curx = mySprite.x
+            cury = mySprite.y
+            mySprite.setPosition(sprites.readDataNumber(mySprite, "prevx"), sprites.readDataNumber(mySprite, "prevy"))
+            repeat = Math.abs(curx - sprites.readDataNumber(mySprite, "prevx")) + Math.abs(cury - sprites.readDataNumber(mySprite, "prevy"))
+            canx = 1
+            cany = 1
+            for (let index = 0; index <= repeat; index++) {
+                if (repeat > 0) {
+                    if (canx == 1) {
+                        mySprite.x += (curx - sprites.readDataNumber(mySprite, "prevx")) / repeat
+                        for (let index2 = 0; index2 <= TileCollisionArrayX.length; index2++) {
+                            if (canx == 1) {
+                                if (TileCollisionArrayX[index2] - mySprite.left >= 0 && TileCollisionArrayX[index2] - mySprite.left < mySprite.width && (TileCollisionArrayY[index2] - mySprite.top >= 0 && TileCollisionArrayY[index2] - mySprite.top < mySprite.height)) {
+                                    if (mySprite.image.getPixel(TileCollisionArrayX[index2] - mySprite.left, TileCollisionArrayY[index2] - mySprite.top) != 0) {
+                                        mySprite.x += 0 - (curx - sprites.readDataNumber(mySprite, "prevx")) / repeat
+                                        canx = 0
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    if (cany == 1) {
+                        mySprite.y += (cury - sprites.readDataNumber(mySprite, "prevy")) / repeat
+                        for (let index22 = 0; index22 <= TileCollisionArrayY.length; index22++) {
+                            if (cany == 1) {
+                                if (TileCollisionArrayX[index22] - mySprite.left >= 0 && TileCollisionArrayX[index22] - mySprite.left < mySprite.width && (TileCollisionArrayY[index22] - mySprite.top >= 0 && TileCollisionArrayY[index22] - mySprite.top < mySprite.height)) {
+                                    if (mySprite.image.getPixel(TileCollisionArrayX[index22] - mySprite.left, TileCollisionArrayY[index22] - mySprite.top) != 0) {
+                                        mySprite.y += 0 - (cury - sprites.readDataNumber(mySprite, "prevy")) / repeat
+                                        cany = 0
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            sprites.setDataNumber(mySprite, "prevx", mySprite.x)
+sprites.setDataNumber(mySprite, "prevy", mySprite.y)
+        }
+    }
 })
