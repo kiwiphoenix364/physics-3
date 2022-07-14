bd92318830a534d963f875c6efaec4c094ce5341
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-07-13 20:57:16 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -12,21 +12,6 @@ let TileCollisionArrayX = [0]
 TileCollisionArrayY = []
 TileCollisionArrayX = []
 namespace MiniTilemaps {
-    //% block="Generate mini tilemap on all $selected tiles with collision $collisionImg"
-    //% selected.shadow=tileset_tile_picker
-    //% collisionImg.shadow=screen_image_picker
-    export function GenerateCollision(selected: Image, collisionImg: Image) {
-        for (let value2 of tiles.getTilesByType(selected)) {
-            for (let index32 = 0; index32 <= collisionImg.width; index32++) {
-                for (let index23 = 0; index23 <= collisionImg.height; index23++) {
-                    if (0 != collisionImg.getPixel(index32, index23)) {
-                        TileCollisionArrayX.push(value2.column * 16 + index32)
-                        TileCollisionArrayY.push(value2.row * 16 + index23)
-                    }
-                }
-            }
-        }
-    }
     //% block="Clear all mini tilemaps"
     export function ClearAll () {
     let TileCollisionArrayX = [0]
